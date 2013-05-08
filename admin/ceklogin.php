8d9d63492f85f73bbf8c74b6a77942a304969068
memble <me@here.com>
2013-05-08 15:00:11 +0700

    update ceklogin

--- admin/ceklogin.php
+++ admin/ceklogin.php
@@ -1,16 +1,17 @@
 <?php
-include "Koneksi.php";
+include "../config/koneksi.php";
 
+const BASE_URL = '/andri';
 $user= $_POST['txtusername'];
 $password= $_POST['txtpassword'];
 $query=mysql_query("select * from login where username='$user' and password='$password'");
-$row= mysql_fetch_row($query);
-if (count($row) > 0){
-//header("location:/test");	
+$row= mysql_fetch_array($query);
 
+if (count($row) > 0){
+    header("location:" . BASE_URL . "/admin/halaman/index.php?halaman=home");	
 }
 else
 {
-echo"test";
+    echo"<script>alert('USERNAME TIDAK DITEMUKAN');</script>";
 }
 ?>
