4d58f26d38876c2a2c5cee49f97e5023a541ca3d
memble <me@here.com>
2013-05-10 13:57:17 +0700

    index extension change

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login Page</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="ceklogin.php">
+
+<table width="1078" border="0">
+  <tr>
+    <td width="113" height="23">username</td>
+    <td width="955"><input type='text' name='txtusername' size='50' /></td>
+  </tr>
+  <tr>
+    <td>password</td>
+    <td><input type='password' name='txtpassword' size='50' /></td>
+  </tr>
+  <tr>
+  <td></td>
+  <td><input name="btnlogin" type="submit" value="Login" /></td>
+  </tr>
+</table>
+
+</form>
+</body>
+</html>
