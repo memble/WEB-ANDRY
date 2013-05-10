bbfb75037d2b7d55b576ded11e4df13aa03419ed
memble <thejems12@gmail.com>
2013-05-10 15:07:48 +0700

    set session login

--- admin/ceklogin.php
+++ admin/ceklogin.php
@@ -9,6 +9,15 @@ $rows=mysql_num_rows($query);
 $data= mysql_fetch_array($query);
 
 if ($rows > 0){
+    session_start();
+        
+        $_SESSION['TUGASWEB']=array();
+        
+        $_SESSION['namauser']     = $data['username'];
+        $_SESSION['passuser']     = $data['password'];
+        
+        $_SESSION['login'] = 1;
+        
     header("location:" . BASE_URL . "/admin/halaman/index.php?halaman=home");	
 }
 else
