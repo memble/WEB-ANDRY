f2a0a3a601d441bd58a6cbd7bf57cf242b1d6c23
memble <thejems12@gmail.com>
2013-05-10 21:19:18 +0700

    extract new database

--- cybercrime.sql
+++ cybercrime.sql
@@ -1,64 +1,61 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 06, 2013 at 04:47 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+# SQL Manager 2010 for MySQL 4.5.0.9
+# ---------------------------------------
+# Host     : localhost
+# Port     : 3306
+# Database : cybercrime
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES latin1 */;
 
---
--- Database: `cybercrime`
---
+SET FOREIGN_KEY_CHECKS=0;
 
--- --------------------------------------------------------
+#
+# Structure for the `artikel` table : 
+#
 
---
--- Table structure for table `artikel`
---
+DROP TABLE IF EXISTS `artikel`;
 
-CREATE TABLE IF NOT EXISTS `artikel` (
+CREATE TABLE `artikel` (
   `id_artikel` int(11) NOT NULL AUTO_INCREMENT,
   `judul_artikel` varchar(50) NOT NULL,
   `tgl_posting` date NOT NULL,
   `content` mediumtext NOT NULL,
   `gambar` varchar(50) NOT NULL,
   PRIMARY KEY (`id_artikel`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `artikel`
---
+#
+# Structure for the `login` table : 
+#
 
-INSERT INTO `artikel` (`id_artikel`, `judul_artikel`, `tgl_posting`, `content`, `gambar`) VALUES
-(1, 'test judul', '2013-04-02', 'test content artikelll...............', '');
+DROP TABLE IF EXISTS `login`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
-
-CREATE TABLE IF NOT EXISTS `login` (
+CREATE TABLE `login` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `login`
---
+#
+# Data for the `artikel` table  (LIMIT 0,500)
+#
 
-INSERT INTO `login` (`username`, `password`) VALUES
-('andri', 'admin');
+INSERT INTO `artikel` (`id_artikel`, `judul_artikel`, `tgl_posting`, `content`, `gambar`) VALUES 
+  (1,'test','2013-05-10','aaa','868great-billboard1.jpeg'),
+  (2,'aaa','2013-05-10','aaaa','964great-billboard1.jpeg');
+COMMIT;
+
+#
+# Data for the `login` table  (LIMIT 0,500)
+#
+
+INSERT INTO `login` (`username`, `password`) VALUES 
+  ('andri','admin');
+COMMIT;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
